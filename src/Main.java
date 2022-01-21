cbbe07ae9f0b7289b6f1965080a7abf275625445
Mikita <haniks2899@gmail.com>
2022-01-21 17:37:42 +0300

    feat(HW_1): added text to 'sout'

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ public class Main {
         int b = 60;
         int c = 0b11101;
         int d = 0b1001;
-        System.out.println(~a);
-        System.out.println(a & b);
-        System.out.println(a | b);
-        System.out.println(a ^ b);
-        System.out.println(b << 4);
-        System.out.println(a >> 1);
-        System.out.println(a >>> 1);
-        System.out.println(c + d);
-        System.out.println(Integer.toBinaryString(c + d));
-        System.out.println(Integer.toBinaryString(a >>> 1));
+        System.out.println("Bitwise 'Complement (DEC)': " + ~a);
+        System.out.println("Bitwise 'AND'(DEC): " + (a & b));
+        System.out.println("Bitwise 'OR' (DEC): " + (a | b));
+        System.out.println("Bitwise 'XOR' (DEC): " + (a ^ b));
+        System.out.println("Bit 'Signed Left shift' (DEC): " + (b << 4));
+        System.out.println("Bit 'Signed Right shift' (DEC): " + (a >> 1));
+        System.out.println("Bit 'Unsigned Right shift' (DEC): " + (a >>> 1));
+        System.out.println("Sum of two numbers in binary (DEC): " + (c + d));
+        System.out.println("Sum of two numbers in binary (BIN): " + Integer.toBinaryString(c + d));
+        System.out.println("Sum of two numbers in binary (BIN): " + Integer.toBinaryString(a >>> 1));
     }
 }
