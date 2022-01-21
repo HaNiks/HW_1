857ccee2631e07235bd68a886509e6c2e7829a59
Mikita <haniks2899@gmail.com>
2022-01-21 18:54:23 +0300

    feat(HW_1): add function 'getBinDec'

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,51 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        getBinDec();
+    }
+
+    public static void getBinDec() {
+        Scanner scanner = new Scanner(System.in);
         int a = -20;
         int b = 60;
         int c = 0b11101;
         int d = 0b1001;
-        String text = "Enter number system ('BIN' or 'DEC'):";
         boolean isRepeat = true;
-        Scanner scanner = new Scanner(System.in);
+        String text = "Enter number system ('BIN' or 'DEC'):";
         while (isRepeat) {
             System.out.println(text);
             String inputText = scanner.nextLine();
             if (inputText.equals("DEC")) {
-                System.out.println("Bitwise 'Complement': " + ~a);
-                System.out.println("Bitwise 'AND': " + (a & b));
-                System.out.println("Bitwise 'OR': " + (a | b));
-                System.out.println("Bitwise 'XOR': " + (a ^ b));
-                System.out.println("Bit 'Signed Left shift': " + (b << 4));
-                System.out.println("Bit 'Signed Right shift': " + (a >> 1));
-                System.out.println("Bit 'Unsigned Right shift': " + (a >>> 1));
-                System.out.println("Sum of two numbers in binary: " + (c + d));
+                decNumberSystem(a, b, c, d);
                 isRepeat = false;
-            }
-            else if (inputText.equals("BIN")) {
-                System.out.println("Sum of two numbers in binary: " + Integer.toBinaryString(c + d));
-                System.out.println("Sum of two numbers in binary: " + Integer.toBinaryString(a >>> 1));
+            } else if (inputText.equals("BIN")) {
+                binNumberSystem(a, b, c, d);
                 isRepeat = false;
-            }
-            else {
+            } else {
                 text = "Enter 'BIN' or 'DEC':";
             }
         }
+    }
+
+    public static void decNumberSystem(int a, int b, int c, int d) {
+        System.out.println("Bitwise 'Complement': " + ~a);
+        System.out.println("Bitwise 'AND': " + (a & b));
+        System.out.println("Bitwise 'OR': " + (a | b));
+        System.out.println("Bitwise 'XOR': " + (a ^ b));
+        System.out.println("Bit 'Signed Left shift': " + (b << 4));
+        System.out.println("Bit 'Signed Right shift': " + (a >> 1));
+        System.out.println("Bit 'Unsigned Right shift': " + (a >>> 1));
+        System.out.println("Sum of two numbers ('c' and 'd') in binary: " + (c + d));
+    }
+
+    public static void binNumberSystem(int a, int b, int c, int d) {
+        System.out.println("Sum of two numbers ('c' and 'd') in binary: " + Integer.toBinaryString(c + d));
+        System.out.println("Bit 'Unsigned Right shift' in binary: " + Integer.toBinaryString(a >>> 1));
+        System.out.println("Bitwise 'Complement': " + Integer.toBinaryString(~a));
+        System.out.println("Bitwise 'AND': " + Integer.toBinaryString(a & b));
+        System.out.println("Bitwise 'OR': " + Integer.toBinaryString(a | b));
+        System.out.println("Bitwise 'XOR': " + Integer.toBinaryString(a ^ b));
+        System.out.println("Bit 'Signed Left shift': " + Integer.toBinaryString(b << 4));
+        System.out.println("Bit 'Signed Right shift': " + Integer.toBinaryString(a >> 1));
     }
 }
