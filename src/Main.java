89e9d507f9464881e7416730de89c43a8fd987aa
Mikita <haniks2899@gmail.com>
2022-01-21 17:56:37 +0300

    feat(HW_1): add construction if-else

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -5,15 +6,31 @@ public class Main {
         int b = 60;
         int c = 0b11101;
         int d = 0b1001;
-        System.out.println("Bitwise 'Complement (DEC)': " + ~a);
-        System.out.println("Bitwise 'AND'(DEC): " + (a & b));
-        System.out.println("Bitwise 'OR' (DEC): " + (a | b));
-        System.out.println("Bitwise 'XOR' (DEC): " + (a ^ b));
-        System.out.println("Bit 'Signed Left shift' (DEC): " + (b << 4));
-        System.out.println("Bit 'Signed Right shift' (DEC): " + (a >> 1));
-        System.out.println("Bit 'Unsigned Right shift' (DEC): " + (a >>> 1));
-        System.out.println("Sum of two numbers in binary (DEC): " + (c + d));
-        System.out.println("Sum of two numbers in binary (BIN): " + Integer.toBinaryString(c + d));
-        System.out.println("Sum of two numbers in binary (BIN): " + Integer.toBinaryString(a >>> 1));
+        String text = "Enter number system ('BIN' or 'DEC'):";
+        boolean isRepeat = true;
+        Scanner scanner = new Scanner(System.in);
+        while (isRepeat) {
+            System.out.println(text);
+            String inputText = scanner.nextLine();
+            if (inputText.equals("DEC")) {
+                System.out.println("Bitwise 'Complement': " + ~a);
+                System.out.println("Bitwise 'AND': " + (a & b));
+                System.out.println("Bitwise 'OR': " + (a | b));
+                System.out.println("Bitwise 'XOR': " + (a ^ b));
+                System.out.println("Bit 'Signed Left shift': " + (b << 4));
+                System.out.println("Bit 'Signed Right shift': " + (a >> 1));
+                System.out.println("Bit 'Unsigned Right shift': " + (a >>> 1));
+                System.out.println("Sum of two numbers in binary: " + (c + d));
+                isRepeat = false;
+            }
+            else if (inputText.equals("BIN")) {
+                System.out.println("Sum of two numbers in binary: " + Integer.toBinaryString(c + d));
+                System.out.println("Sum of two numbers in binary: " + Integer.toBinaryString(a >>> 1));
+                isRepeat = false;
+            }
+            else {
+                text = "Enter 'BIN' or 'DEC':";
+            }
+        }
     }
 }
